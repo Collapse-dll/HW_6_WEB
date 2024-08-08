9d389ac8c386c038491851a5f0ddc1e88fdf3fad
Collapse_dll <rassskoll@gmail.com>
2024-08-08 12:26:51 +0200

    first commit

---
+++ select_from_db.py
@@ -0,0 +1,36 @@
+import logging
+import os
+from psycopg2 import DatabaseError
+from connect import create_connection
+
+if __name__ == "__main__":
+    query_01 = """
+    SELECT s.id,
+        s.first_name,
+        s.last_name,
+        AVG(m.mark) as average_mark
+    FROM students as s
+    JOIN marks as m ON s.id = m.student_id  
+    GROUP BY s.id
+    ORDER BY average_mark ASC
+    LIMIT 5;
+    """
+
+    try:
+        with create_connection() as connection:
+            if connection is not None:
+                c = connection.cursor()
+
+                sql_files = [file for file in os.listdir() if file.endswith(".sql")]
+                try:
+                    c.execute(query_01)
+                    result = c.fetchall()
+                    print(result)
+                except DatabaseError as error:
+                    logging.error(error)
+                finally:
+                    c.close()
+            else:
+                print("Error ! Cannot create the database connection !!!")
+    except RuntimeError as err:
+        logging.error(err)
---
+++ select_from_db_list.py
@@ -0,0 +1,33 @@
+import logging
+import os
+
+from psycopg2 import DatabaseError
+
+from connect import create_connection
+
+
+if __name__ == "__main__":
+    try:
+        with create_connection() as connection:
+            if connection is not None:
+                c = connection.cursor()
+
+                sql_files = [file for file in os.listdir() if file.endswith(".sql")]
+
+                try:
+                    for file_path in sql_files:
+                        with open(file_path, "r", encoding="utf-8") as file:
+                            sql_query = file.read()
+
+                        print("===" * 50)
+                        c.execute(sql_query)
+                        result = c.fetchall()
+                        print(result)
+                except DatabaseError as error:
+                    logging.error(error)
+                finally:
+                    c.close()
+            else:
+                print("Error ! Cannot create the database connection !!!")
+    except RuntimeError as err:
+        logging.error(err)
